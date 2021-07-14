6eb75e363b7d62fc711fedc1088065e32c43c7ac
Frank <frj071196@hotmail.com>
2021-07-14 13:12:39 -0500

    Initial commit with base BMI calculator component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {BMICalculator} from './Components/BMICalculator';
+
+function App() {
+  return (
+    <BMICalculator/>
+  );
+}
+
+export default App;
